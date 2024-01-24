ec5b53876380d70f2feaa82ba150e585e717c5dd
Lucas <lucasduartedeveloper@gmail.com>
2024-01-24 14:35:43 -0300

    first commit

--- script/frequency-draw.js
+++ script/frequency-draw.js
@@ -689,7 +689,7 @@ var visibilityChange;
 if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
   visibilityChange = "visibilitychange";
 } else if (typeof document.msHidden !== "undefined") {
-  visibilityChange = "msvisivbilitychange";
+  visibilityChange = "msvisibilitychange";
 } else if (typeof document.webkitHidden !== "undefined") {
   visibilityChange = "webkitvisibilitychange";
 }
