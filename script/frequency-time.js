77b60efaa1f762bb7732ddea48166ed1ceb8288a
Lucas <lucasduartedeveloper@gmail.com>
2024-02-21 14:19:06 -0300

    first commit

--- script/frequency-time.js
+++ script/frequency-time.js
@@ -3,6 +3,7 @@ var warningBeep = new Audio("audio/warning_beep.wav");
 
 var sw = 360; //window.innerWidth;
 var sh = 669; //window.innerHeight;
+var swo = sw-100;
 
 var gridSize = 10;
 
@@ -25,7 +26,7 @@ var backgroundColor = "rgba(50,50,65,1)";
 var buttonColor = "rgba(75,75,90,1)";
 
 var audioStream = 
-new Audio("audio/music/stream-0.wav");
+new Audio("audio/music/ringtone-0.wav");
 
 // Botão de gravação
 $(document).ready(function() {
@@ -47,6 +48,20 @@ $(document).ready(function() {
     };
 
     tileSize = (sw/7);
+
+    backgroundView = document.createElement("img");
+    backgroundView.style.position = "absolute";
+    //pictureView.style.background = "#fff";
+    backgroundView.style.objectFit = "cover";
+    backgroundView.width = (sw);
+    backgroundView.height = (sh); 
+    backgroundView.style.left = (0)+"px";
+    backgroundView.style.top = (0)+"px";
+    backgroundView.style.width = (sw)+"px";
+    backgroundView.style.height = (sh)+"px";
+    backgroundView.style.zIndex = "15";
+    //backgroundView.src = "img/background-0.png";
+    //document.body.appendChild(backgroundView);
 
     camera = document.createElement("video");
     camera.style.position = "absolute";
@@ -77,7 +92,7 @@ $(document).ready(function() {
 
     text = "";
     direction = -1;
-    lineHeight = (sw);
+    lineHeight = (swo/4);
     perfectMargin = 0.01;
 
     pause = false;
@@ -92,13 +107,20 @@ $(document).ready(function() {
 
     userInteracted = false;
 
+    poorCount = 0;
+    goodCount = 0;
+    greatCount = 0;
+    perfectCount = 0;
+
     pictureView.ontouchstart = function(e) {
         if (userInteracted && oscillator.paused) {
             oscillator.start();
             oscillator.paused = false;
         }
 
-        direction = Math.floor((e.touches[0].clientX)/(sw/4));
+        if (e.touches[0].clientX < 100) return;
+
+        direction = Math.floor((e.touches[0].clientX-100)/(swo/4));
 
         var thereIsObject = false;
         for (var n = 0; n < positionArr.length; n++) {
@@ -168,6 +190,8 @@ $(document).ready(function() {
                 objX = lineArr[obj.direction];
                 objY = obj.y;
 
+                audioStream.pause();
+
                 break;
             }
             else if (obj.direction == direction && 
@@ -179,7 +203,22 @@ $(document).ready(function() {
                 else text = "POOR";
 
                 //sfxPool.play("audio/"+(text.toLowercase())+".wav");
-                oscillator.frequency.value = (1-hit)*50;
+                //oscillator.frequency.value = (1-hit)*250;
+
+                switch (text) {
+                    case "POOR":
+                        poorCount += 1;
+                        break;
+                    case "GOOD":
+                        goodCount += 1;
+                        break;
+                    case "GREAT":
+                        greatCount += 1;
+                        break;
+                    case "PERFECT":
+                        perfectCount += 1;
+                        break;
+                }
 
                 clearTimeout(clearTextTimeout);
                 clearTextTimeout= setTimeout(function() {
@@ -192,6 +231,8 @@ $(document).ready(function() {
                 startY = ((sh/2)+(sh/4));
                 objX = lineArr[obj.direction];
                 objY = obj.y;
+
+                audioStream.play();
 
                 positionArr = positionArr.filter((o) => { return !o.remove; });
                 break;
@@ -239,6 +280,9 @@ var animate = function() {
     }
     renderTime = new Date().getTime();
 
+    if (audioStream.currentTime > 7)
+    audioStream.currentTime = 0;
+
     if (!pause)
     requestAnimationFrame(animate);
 };
@@ -247,10 +291,10 @@ var positionArr = [];
 
 var directionArr = [  -90, 0, -270, -180 ];
 var lineArr = [ 
-    (sw/4)-(sw/8), 
-    ((sw/4)*2)-(sw/8), 
-    ((sw/4)*3)-(sw/8), 
-    ((sw/4)*4)-(sw/8) 
+    (swo/4)-(swo/8), 
+    ((swo/4)*2)-(swo/8), 
+    ((swo/4)*3)-(swo/8), 
+    ((swo/4)*4)-(swo/8) 
 ];
 
 var drawButton = function(ctx, x, y, size, color, direction) {
@@ -282,33 +326,57 @@ var drawImage = function() {
     ctx.clearRect(0, 0, sw, sh);
 
     ctx.fillStyle = "#000";
-    ctx.fillRect(0, 0, sw, sh);
+    //ctx.fillRect(0, 0, sw, sh);
 
     ctx.lineWidth = 1;
-    ctx.strokeStyle = "#5f5";
+    ctx.strokeStyle = "#555";
 
     ctx.beginPath();
     ctx.moveTo(0, ((sh/2)+(sh/4))-(lineHeight));
     ctx.lineTo(sw, ((sh/2)+(sh/4))-(lineHeight));
     ctx.stroke();
 
-    ctx.strokeStyle = "#ff5";
+    ctx.fillStyle = "#fff";
+    ctx.font = "10px sans serif";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+    ctx.fillText("POOR: "+poorCount, 0, ((sh/2)+(sh/4))-(lineHeight));
+
     ctx.beginPath();
     ctx.moveTo(0, ((sh/2)+(sh/4))-(lineHeight*0.75));
     ctx.lineTo(sw, ((sh/2)+(sh/4))-(lineHeight*0.75));
     ctx.stroke();
 
-    ctx.strokeStyle = "#55f";
+    ctx.fillStyle = "#fff";
+    ctx.font = "10px sans serif";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+    ctx.fillText("GOOD: "+goodCount, 
+    0, ((sh/2)+(sh/4))-(lineHeight*0.75));
+
     ctx.beginPath();
     ctx.moveTo(0, ((sh/2)+(sh/4))-(lineHeight*0.25));
     ctx.lineTo(sw, ((sh/2)+(sh/4))-(lineHeight*0.25));
     ctx.stroke();
 
-    ctx.strokeStyle = "#555";
+    ctx.fillStyle = "#fff";
+    ctx.font = "10px sans serif";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+    ctx.fillText("GREAT: "+greatCount, 
+    0, ((sh/2)+(sh/4))-(lineHeight*0.25));
+
     ctx.beginPath();
     ctx.moveTo(0, ((sh/2)+(sh/4))-(lineHeight*(0+perfectMargin)));
     ctx.lineTo(sw, ((sh/2)+(sh/4))-(lineHeight*(0+perfectMargin)));
     ctx.stroke();
+
+    ctx.fillStyle = "#fff";
+    ctx.font = "10px sans serif";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+    ctx.fillText("PERFECT: "+perfectCount, 0, 
+    ((sh/2)+(sh/4))-(lineHeight*(0+perfectMargin)));
 
     var y = (sh/2)+(sh/4);
 
@@ -326,11 +394,13 @@ var drawImage = function() {
             startY = ((sh/2)+(sh/4));
             objX = lineArr[obj.direction];
             objY = obj.y;
+
+            audioStream.pause();
         }
 
         var x = lineArr[obj.direction];
         var color = obj.highlight ? "#5f5" : "#777";
-        drawButton(ctx, x, obj.y, (sw/4)-10, color, obj.direction);
+        drawButton(ctx, 100+x, obj.y, (swo/4)-10, color, obj.direction);
 
         obj.y += 1;
     }
@@ -342,27 +412,29 @@ var drawImage = function() {
         direction == 3 ? "#fff" : "#555"
     ];
 
-    drawButton(ctx, (sw/4)-(sw/8), y, (sw/4)-5, colorArr[0], 0);
-    drawButton(ctx, ((sw/4)*2)-(sw/8), y, (sw/4)-5, colorArr[1], 1);
-    drawButton(ctx, ((sw/4)*3)-(sw/8), y, (sw/4)-5, colorArr[2], 2);
-    drawButton(ctx, ((sw/4)*4)-(sw/8), y, (sw/4)-5, colorArr[3], 3);
+    drawButton(ctx, 100+(swo/4)-(swo/8), y, (swo/4)-5, colorArr[0], 0);
+    drawButton(ctx, 
+    100+((swo/4)*2)-(swo/8), y, (swo/4)-5, colorArr[1], 1);
+    drawButton(ctx, 
+    100+((swo/4)*3)-(swo/8), y, (swo/4)-5, colorArr[2], 2);
+    drawButton(ctx, 
+    100+((swo/4)*4)-(swo/8), y, (swo/4)-5, colorArr[3], 3);
 
     ctx.fillStyle = "#fff";
     ctx.font = "25px sans serif";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
+    ctx.fillText(text, (sw/2), (sh/4));
 
     if (pause) {
         ctx.lineWidth = 1;
         ctx.strokeStyle = "#5f5";
 
         ctx.beginPath();
-        ctx.moveTo(startX, startY);
-        ctx.lineTo(objX, objY);
+        ctx.moveTo(100+startX, startY);
+        ctx.lineTo(100+objX, objY);
         ctx.stroke();
     }
-
-    ctx.fillText(text, (sw/2), (sh/4));
 };
 
 var visibilityChange;
